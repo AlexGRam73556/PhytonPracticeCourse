5961b9df56810ca94063f03c4bc2d5a87dcc09d5
Emilio Alejandro Ramírez Gaytán <erg73556@udelasalle.edu.mx>
2023-09-26 19:42:27 -0600

    Suma de 2 numeros

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("Hello World")
+two_digit_number = input("Type a two digit number: ")
+
+first_digit = int(two_digit_number[0])
+second_digit = int(two_digit_number[1])
+
+#print(first_digit)
+#print(second_digit)
+
+result = first_digit + second_digit
+#result = int(first_digit) + int(second_digit)
+print(result)
