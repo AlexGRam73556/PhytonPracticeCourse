0321b6fd508e18286298472ab188ce778869816a
Emilio Alejandro Ramírez Gaytán <erg73556@udelasalle.edu.mx>
2023-09-26 20:04:14 -0600

    Calculadora de BMI

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-two_digit_number = input("Type a two digit number: ")
+#Calcualar el BMI - FORMULA: BMI = WHEIGHT / HEIGHT **2
 
-first_digit = int(two_digit_number[0])
-second_digit = int(two_digit_number[1])
+heightlab = input("Inserta la altura en m: ")
+weightlab = input("Inserta el peso en kg: ")
 
-#print(first_digit)
-#print(second_digit)
+height = float(heightlab)
+weight = int(weightlab)
 
-result = first_digit + second_digit
-#result = int(first_digit) + int(second_digit)
-print(result)
+bmi = weight / height ** 2
+
+print(int(bmi))
